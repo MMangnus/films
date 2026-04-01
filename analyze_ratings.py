5ea9bed341e872d532186df57d73e755584b5fc8
Margot Mangnus <mangnusm@gmail.com>
2026-04-01 23:39:04 +0200

    added todos for matching trope to rating and number of occurences

--- analyze_ratings.py
+++ analyze_ratings.py
@@ -45,9 +45,23 @@ def main():
     lb_data = pd.read_csv(file, sep = ",")
     movie_titles = lb_data["URLSafeNames"]
 
+    movie_titles = movie_titles[:5]
+
     print(f"n. movies {len(movie_titles)}")
     tropes = get_tropes_for_movies(movie_titles)
     print(f"n. tropes found: {len(tropes)}")
 
+    #trope_stats = pd.DataFrame(columns=["tropes","n_occurences","all_ratings"])
+
+    for movie, m_tropes in tropes.items():
+        print(movie, m_tropes)
+        #i_data = list(movie_titles).index(movie)
+        #rating = lb_data.loc[i_data, "Rating"]
+    
+    # TODO: 1. create list with ratings and list with lists of tropes
+    # 2. loop over those lists and create a new dictionairy with key = trope, value = (number of occurences, cumulative rating)
+
+
 if __name__ == "__main__":
     main()
+
